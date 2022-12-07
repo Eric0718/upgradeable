c902f91e55e2955d18ff20f6a2fc550c0e4d2b78
Eric0718 <1434797171@qq.com>
2022-12-07 15:28:18 +0800

    update perpetual machine code

--- contracts/perpetualMachine/perpetualMachine.sol
+++ contracts/perpetualMachine/perpetualMachine.sol
@@ -4,43 +4,115 @@ pragma solidity 0.6.12;
 import "../lib/math/SafeMath.sol";
 import "../lib/access/OwnableUpgradeable.sol";
 import "../lib/verify/Verify.sol";
+import "../interfaces/IERC20.sol";
 
 contract PerpetualMachine is Verify{
-    address public _usdt;
-    address public _uft;
+    using SafeMath for uint256;
+    IERC20 public _usdt;
+    IERC20 public _uft;
     Verify public _vContract;
-    address public _uftReceiver;
-    address public 
-
+    address public _uftSender;
 
     event Participate(address user,uint256 amount);
+    event ClaimCommission(address user,uint256 amount);
+    event ClaimUsdtRewards(address user,uint256 amount);
 
-    function initialize(address usdt,address uft,Verify vContract,address receiver) public initializer {
-        _usdt = usdt;
-        _uft= uft;
+    function initialize(address usdt,address uft,Verify vContract,address usender) public initializer {
+        _usdt = IERC20(usdt);
+        _uft= IERC20(uft);
         _vContract = vContract;
-        _uftReceiver = receiver;
+        _uftSender = usender;
         
         __Ownable_init();
     }
 
     function participate(uint256 uAmount) external returns (bool){
         require(uAmount >0,"Amount cant't be 0");
-        require(_usdt.allowance(msg.sender,this.address) >= uAmount,"spender allowance not enough!");
-        require(_usdt.balanceOf(msg.sender) >= uAmount,"user balance not enough!");
-
-        bool ok = _usdt.transferFrom(msg.sender,this.address,uAmount);
-        if ok{
-            emit Participate(msg.sender,uAmount);
-        }
-        return ok
+        require(
+            _usdt.allowance(msg.sender,address(this)) >= uAmount,
+            "spender allowance not enough!"
+        );
+        require(_usdt.balanceOf(msg.sender) >= uAmount,
+            "user balance not enough!"
+        );
+        require(
+            _usdt.transferFrom(msg.sender,address(this),uAmount),
+            "transfer usdt failed!"
+        ); 
+        emit Participate(msg.sender,uAmount);
+        return true;
     }
 
-    function claimInviteCommission(uint256 value)external returns(bool){
+    function claimUsdt (uint256 value,string calldata signTime, bytes32 signature,bool commission)external returns(bool){
+        require(value > 0,"claim amount can't be 0!");
+        //verify signature
+        require(
+            _vContract.verifySignature(value,signTime,msg.sender,signature),
+            "verify signature failed!"
+        );
         //消耗30%UFT 手续费
+        if (commission){
+            require(
+                serviceCharge(value),
+                "Claim Invite Commission failed!"
+            );
+        }
         //领取value USDT
+        require(_usdt.balanceOf(address(this)) >= value,
+            "contract usdt balance not enough!"
+        );
+        require(
+            _usdt.transferFrom(address(this),msg.sender,value),
+            "transfer usdt failed!"
+        );
+        if (commission){
+            emit ClaimCommission(msg.sender,value);
+        }else{
+            emit ClaimUsdtRewards(msg.sender,value);
+        }
+        
+        return true;
     }
-    function claimUsdtRewards(uint256 value)external returns(bool){
+    
+    function serviceCharge(uint256 value)internal returns(bool){
+        require(value > 0,"claim amount can't be 0!");
+        uint256 charge = value.mul(30).div(100);
+        require(
+            _uft.allowance(msg.sender,address(this)) >= charge,
+            "spender uft allowance not enough!"
+        );
+        require(
+            _uft.balanceOf(msg.sender) >= charge,
+            "Not enough handling fees!"
+        );
+        require(
+            _uft.transferFrom(msg.sender,address(this),charge),
+            "transfer uft failed!"
+        );
+        return true;
+    }
 
+    function claimMintUft(uint256 amount,string calldata signTime, bytes32 signature)external returns(bool){
+        require(amount > 0,"claim amount can't be 0!");
+        //verify signature
+        require(
+            _vContract.verifySignature(amount,signTime,msg.sender,signature),
+            "verify signature failed!"
+        );
+        require(
+            _uft.allowance(_uftSender,address(this)) >= amount,
+            "spender uft allowance not enough!"
+        );
+        require(
+            _uft.balanceOf(_uftSender) >= amount,
+            "Not enough handling fees!"
+        );
+        require(
+            _uft.transferFrom(_uftSender,msg.sender,amount),
+            "transfer uft failed!"
+        );
+        return true;
     }
+    //withDraw USDT
+    //withDraw UFT
 }
